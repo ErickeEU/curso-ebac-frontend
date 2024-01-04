5314bb0996b0187a809741bf730fcf2a63ed4331
Ueric Azevedo <uerickalmeida@gmail.com>
2024-01-04 09:38:36 -0300

    first commit

---
+++ Js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    $('#telefone').mask('(00) 00000-0000');
+    $('#cpf').mask('000.000.000-00');
+    $('#cep').mask('00000-000');
+    $('form').validate({ 
+        rules: {
+            nome: {
+                required: true,
+                rangelength: [3, 30]
+            },
+            email: {
+                required: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true,
+                minlength: 11 
+            }, 
+            endereço: {
+                required: true
+            },
+            cep: {
+                required: true,
+                minlength: 7
+            }
+        },
+        messages: {
+            nome: 'Por favor, preencha o nome completo.',
+            email: 'Por favor, insira um e-mail.',
+            telefone: 'Por favor, insira um número de telefone.',
+            cpf: 'Por favor, insira um cpf.',
+            endereço: 'Por favor, insira um endereço.',
+            cep: 'Por favor, insira um cep.'
+        },
+        submitHandler: function(form) {
+            $('#sucess-message').attr('class', 'sucess')
+        },
+        invalidHandler: function(event, validator) {
+            let incorretFields = validator.numberOfInvalids()
+            if (incorretFields) {
+                alert(`${incorretFields} campos estão incorretos.`)
+            } 
+        }
+    })
+})
